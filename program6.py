a8985ccf01b3ad853c86ace4c0c8787d0309670f
Souvik Kumar Pramanik <123581988+souvikpramanikgit@users.noreply.github.com>
2026-02-09 00:23:25 +0530

    Update program6.py

--- program6.py
+++ program6.py
@@ -1,1 +1,17 @@
-
+# cook your dish here
+t=int(input())
+for i in range(t):
+    n,a,b=map(int,input().split())
+    arr=list(map(int,input().split()))
+    wearing=False
+    count=0
+    for j in range(n):
+        num=arr[j]
+        if num<a:
+            if not wearing:
+                count+=1
+                wearing=True
+        elif num>b:
+            wearing=False
+    print(count)
+    
