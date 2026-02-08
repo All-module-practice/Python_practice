a9a5703fd27f17935f5785d799b061f13c120930
Souvik Kumar Pramanik <123581988+souvikpramanikgit@users.noreply.github.com>
2026-02-09 00:25:20 +0530

    Update program6.py

--- program6.py
+++ program6.py
@@ -1,4 +1,21 @@
-# cook your dish here
+```
+Correct Logic (think in states 🧥)
+
+Initially: wearing = False
+
+For each day:
+
+If T < A
+→ Chef must wear jacket
+→ If not wearing already → count++
+
+If T > B
+→ Chef must remove jacket
+
+If A ≤ T ≤ B
+→ Chef can keep whatever he’s wearing (do nothing)
+```
+
 t=int(input())
 for i in range(t):
     n,a,b=map(int,input().split())
