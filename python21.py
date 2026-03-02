ee7590e444fdbc37668cb6a54b67afa818b5c79e
Souvik Kumar Pramanik <123581988+souvikpramanikgit@users.noreply.github.com>
2026-03-03 00:01:00 +0530

    Update python21.py

--- python21.py
+++ python21.py
@@ -3,3 +3,11 @@
 # We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 # The input string will only consist of lower case letters and/or spaces.
+
+def get_count(sentence):
+    count=0
+    for i in sentence:
+        if i in 'aeiou':
+            count+=1
+    return count
+#     pass
