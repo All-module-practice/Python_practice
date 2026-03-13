64fd7a4efcc7a16d880f0b927484fdf06e4e0c7e
Souvik Kumar Pramanik <123581988+souvikpramanikgit@users.noreply.github.com>
2026-03-13 23:59:09 +0530

    Create python29.py

---
+++ python29.py
@@ -0,0 +1,14 @@
+# The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+# What if the string is empty? Then the result should be empty object literal, {}.
+
+
+def count(s):
+    # The function code should be here
+    dict={}
+    for i in s:
+        if i in dict:
+            dict[i]+=1
+        else:
+            dict[i]=1
+    return dict
