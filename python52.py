bd27fd6826c999e18cab71be1cf31979f4907853
Souvik Kumar Pramanik <123581988+souvikpramanikgit@users.noreply.github.com>
2026-04-11 00:01:11 +0530

    Implement function to double array values
    Added a function to double the values in an array.

--- python52.py
+++ python52.py
@@ -1,1 +1,12 @@
-# program
+# Given an array of integers, return a new array with each value doubled.
+
+# For example:
+
+# [1, 2, 3] --> [2, 4, 6]
+
+def maps(a):
+    new_arr=[]
+    for i in a:
+        new_arr.append(i*2)
+    return new_arr
+    pass
